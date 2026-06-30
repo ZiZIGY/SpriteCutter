a164516f301e27f019b805a4e916dfe2f882e012
Zhukov Alexandr <al.zhukov@akrikhin.ru>
2026-06-30 20:25:12 +0300

    fix path

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
-  base: '/GLSLAudioVisualizer/',
+  base: '/SpriteCutter/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
